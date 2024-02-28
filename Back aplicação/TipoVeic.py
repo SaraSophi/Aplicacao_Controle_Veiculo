0e2a5af6780d246e19ae9f233c77a7dc5e5935ac
SaraSophi <117236731+SaraSophi@users.noreply.github.com>
2024-02-28 11:52:28 -0300

    Add files via upload

--- Back aplicação/TipoVeic.py
+++ Back aplicação/TipoVeic.py
@@ -1,3 +1,4 @@
+
 class TipoVeic:
     def __init__(self, cod, descricao, categoria):
         self.cod = cod    
@@ -27,23 +28,28 @@ class GerenciadorTipoVeic:
                 return True
         return False
 
-# Exemplo de uso:
+    def listarTiposVeic(self):
+        return [tipoVeic.descricao for tipoVeic in self.tiposVeic]
+
 gerenciador = GerenciadorTipoVeic()
 
-# Inserindo tipos de veículos
-gerenciador.inserirTipoVeic(1, 'Caminhão', 'Pesado')
-gerenciador.inserirTipoVeic(2, 'Utilitário', 'Leve')
+# Adicionando os tipos de veículos
+gerenciador.inserirTipoVeic(1, 'Truck',             'Pesado')
+gerenciador.inserirTipoVeic(2, 'Utilitário',        'Leve'  )
+gerenciador.inserirTipoVeic(3, 'Toco',              'Pesado')
+gerenciador.inserirTipoVeic(4, 'Carreta',           'Pesado')
+gerenciador.inserirTipoVeic(5, 'Bitrem',            'Pesado')
+gerenciador.inserirTipoVeic(6, 'Caminhão',          'Pesado')
+gerenciador.inserirTipoVeic(7, 'Bi-truck',          'Pesado')
+gerenciador.inserirTipoVeic(8, 'Rodotrem Dolly',    'Pesado')
 
-# Excluindo um tipo de veículo
+
+# Validando a exclusão 
 if gerenciador.excluirTipoVeic(2):
     print("Tipo de veículo excluído com sucesso.")
 else:
     print("Tipo de veículo não encontrado.")
 
-# Editando um tipo de veículo
-if gerenciador.editarTipoVeic(1, 'Caminhão de Carga', 'Pesado'):
-    print("Tipo de veículo editado com sucesso.")
-else:
-    print("Tipo de veículo não encontrado ou dados inválidos.")
- 
-print(f"Disponiveis: ", GerenciadorTipoVeic)
+
+# Listando tipos de veículos disponíveis
+print("Disponíveis:", gerenciador.listarTiposVeic())
