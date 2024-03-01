ca958fb6602db4ce7f5024b12662ae4bd276d30b
KaikyLima <kaikylimasouza@gmail.com>
2024-03-01 17:36:12 -0300

    banco update

--- connection.py
+++ connection.py
@@ -1,7 +1,9 @@
 from urllib.parse import quote
-from sqlalchemy import create_engine, text, select
+from sqlalchemy import create_engine, text, select, MetaData, Table, Update
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 import cx_Oracle
+
 # conn = cx_Oracle.connect('system/ksl1708@localhost')
 
 lib_dir = "C:\oracle\instantclient_21_13"
@@ -20,10 +22,35 @@ engine = create_engine(url = instance, echo=True, max_identifier_length=30)
 
 db_session = scoped_session(sessionmaker(bind=engine, autoflush=True, autocommit=False))
 
+'''
+uf = Table('uf', MetaData(bind=engine), autoload=True)
+
+try:
+   with db_session.begin():
+      stmt=Update(uf).where(uf)
+      db_session.execute(stmt)
+      db_session.commit()
+   print("sucesso")
+
+except Exception as e:
+    # Em caso de erro, faça o rollback da transação
+    db_session.rollback()
+    print("Erro durante a atualização:", e)
+finally:
+    # Feche a sessão
+    db_session.close()
 # Definir a consulta SQL
 sql = "SELECT * FROM uf"
 
+'''
+
 # Executar a consulta
+response = db_session.execute(text('SELECT * FROM uf'))
+for row in response:
+ print(row)
+
+
+
 # cursor = conn.cursor()
 # cursor.execute(sql)
 
@@ -35,9 +62,6 @@ sql = "SELECT * FROM uf"
 # cursor.close()
 # conn.close()
 
-response = db_session.execute(text('SELECT * FROM teste'))
-for row in response:
-   print(row)
 
 
 
