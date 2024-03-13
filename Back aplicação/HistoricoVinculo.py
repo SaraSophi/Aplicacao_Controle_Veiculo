2cf03e305ac4daf4b26f9c4cd0013ae7487b2a04
Kaikyl <kaikylimasouza@gmail.com>
2024-03-12 21:29:25 -0300

    modelo logico

--- Back aplicação/HistoricoVinculo.py
+++
@@ -1,7 +0,0 @@
-
-# a fins de formalização
-class HistoricoVinculo:
-    def __init__(self, cod, descricao, categoria):
-        self.cod        = cod    
-        self.descricao  = descricao
-        self.categoria  = categoria
